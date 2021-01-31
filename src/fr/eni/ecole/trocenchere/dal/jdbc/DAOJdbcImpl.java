2f135ed6b5e87c0500905c739cfdd0ff7518b30f
aline <marchal.aline@gmail.com>
2021-01-31 18:34:32 +0100

    Commit - effacer var tests

--- src/fr/eni/ecole/trocenchere/dal/jdbc/DAOJdbcImpl.java
+++ src/fr/eni/ecole/trocenchere/dal/jdbc/DAOJdbcImpl.java
@@ -3,8 +3,5 @@ package fr.eni.ecole.trocenchere.dal.jdbc;
 import fr.eni.ecole.trocenchere.dal.DAO;
 
 public class DAOJdbcImpl implements DAO{
-	
-	private int test2 = 2;
-	private int test3 = 3;
 
 }
