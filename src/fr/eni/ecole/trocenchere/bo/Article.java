6669a47a4d3dade7d34ece3e76c7a4132e217c21
aline <marchal.aline@gmail.com>
2021-02-01 12:38:25 +0100

    test branche

--- src/fr/eni/ecole/trocenchere/bo/Article.java
+++ src/fr/eni/ecole/trocenchere/bo/Article.java
@@ -7,6 +7,7 @@ public class Article implements Serializable {
 	private static final long serialVersionUID = -4813349171372997274L;
 	
 	private String test;
+	private String testBranche;
 	
 	/**
 	 * Commentaire test commit 01/02/2021 11:30
