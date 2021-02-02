5ddd6e6a88fc7b730b9cb2da292f4d1757759888
Oscar Van Buren <erwan.leroy2020@campus-eni.fr>
2021-02-02 21:51:02 +0100

    Creation servlet ConnectedHome (squelette)

---
+++ src/fr/eni/ecole/trocenchere/servlets/ServletConnectedHome.java
@@ -0,0 +1,36 @@
+package fr.eni.ecole.trocenchere.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletConnectedHome
+ */
+@WebServlet("/ServletConnectedHome")
+public class ServletConnectedHome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// display Connected Home screen
+				RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/ConnectedHome.jsp");
+				rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
