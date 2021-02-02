94bd1629ddc383bea6f2b2e074b11ed34e76b934
JJJJ <juliette.malgouyard2020@campus-eni.fr>
2021-02-02 18:50:07 +0100

    create user

--- src/fr/eni/ecole/trocenchere/servlets/ServletSignUp.java
+++ src/fr/eni/ecole/trocenchere/servlets/ServletSignUp.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.ecole.trocenchere.bll.UserManager;
+
 /**
  * Servlet implementation class ServletSignUp
  */
@@ -21,6 +23,7 @@ public class ServletSignUp extends HttpServlet {
 	 */
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// display sign-up screen
 		RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/SignUpForm.jsp");
 		rd.forward(request, response);
 	}
@@ -31,6 +34,38 @@ public class ServletSignUp extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		// 1 - get the parameters
+		String user = request.getParameter("userName");
+		String password = request.getParameter("password");
+		String name = request.getParameter("name");
+		String firstName = request.getParameter("firstName");
+		String email = request.getParameter("email");
+		String phone = request.getParameter("phone");
+		String street = request.getParameter("street");
+		String postCode = request.getParameter("postCode");
+		String city = request.getParameter("city");
+
+		// 2 - encrypt password
+		String passwordEncrypted = encrypt(password);
+
+		// 3 - create user
+		UserManager userManager = new UserManager();
+		userManager.addUser(String user, String name, String firstName, String email, String phone, String street,
+				String postCode, String city, String passwordEncrypted);
+
+		// 4 - if everything OK, redirect to Home
+		request.getServletContext().getRequestDispatcher("/ServletConnectedHome").forward(request, response);
+	}
+
+	// function to encrypt password
+	private String encrypt(String password) {
+		String passwordEncrypted = "";
+		for (int i = 0; i < password.length(); i++) {
+			int c = password.charAt(i) ^ 48;
+			passwordEncrypted = passwordEncrypted + (char) c;
+		}
+
+		return passwordEncrypted;
 	}
 
 }
