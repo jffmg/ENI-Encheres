b5c5ba24d2d84807821463f87eb8157b650506ff
JJJJ <juliette.malgouyard2020@campus-eni.fr>
2021-02-03 18:55:52 +0100

    test pour aline

--- src/fr/eni/ecole/trocenchere/servlets/ServletConnectedHome.java
+++ src/fr/eni/ecole/trocenchere/servlets/ServletConnectedHome.java
@@ -22,6 +22,7 @@ import fr.eni.ecole.trocenchere.gestion.erreurs.BusinessException;
 public class ServletConnectedHome extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static List<Integer> listeCodesErreur = new ArrayList<>();
+	private static String test = "test";
 
 	@Override
 	public void init() throws ServletException {
